4db014f27b9bf77651ece9d4e1bdb4555f825056
stefandrazic <stefan.drazic351@gmail.com>
2023-11-03 16:00:26 +0100

    Dodati komentari

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+//Selektovani divovi u HTML-u i dodato dugme za reload stranice
+//Odredjujemo broj za svaki par, pravimo niz za brojeve, kartice, okrenute kartice i broj parova.
+
 const app = document.querySelector("#app");
 const endGame = document.querySelector("#endGame");
 const start = document.querySelector("#start").addEventListener("click", () => {
@@ -10,6 +13,8 @@ const cards = [];
 let totalPairs = totalCards / 2;
 let faceUpCards = [];
 
+//Pravimo klasu za karticu da izbegnemo ponavljanje koda. U konstruktoru odredjujemo boju kartica i broj koji ce sadrzati. isFaceUp nam govori da li je kartica okrenuta.
+
 class Cards {
   element = null;
 
@@ -19,6 +24,8 @@ class Cards {
     this.isFaceUp = false;
     this.init();
   }
+
+  //Odredjujemo osnovne stilove kartice, prikazujemo je unutar aplikacije i dodajemo eventListener na klik ako nije vec kliknuta i ako nisu aktivno okrenute vise od 2 kartice. Menja se isFaceUp varijabla u true, ispisuje broj u kartici, dodaje se u niz trenutno okrenutih kartica. Kada su okrenute 2 kartice, nakon sekunde proveravamo da li se okrenute kartice podudaraju.
 
   init() {
     this.element = document.createElement("div");
@@ -40,6 +47,8 @@ class Cards {
     app.appendChild(this.element);
   }
 }
+
+//Funkcija za proveru podudaranja kartica u nizu okrenutih kartica. Ako se kartice podudaraju, sakrivamo ih, praznimo niz aktivnih kartica i smanjujemo parove za 1. Ukoliko se ne podudaraju, brisemo sve prikaze brojeva kartica i praznimo niz kartica. Ukoliko brojac parova dodje do 0, brisemo sve kartice sa ekrana i prikazujemo div za kraj igre.
 
 function checkForMatch() {
   if (faceUpCards[0].element.innerHTML === faceUpCards[1].element.innerHTML) {
@@ -63,12 +72,16 @@ function checkForMatch() {
   }
 }
 
+//Funkcija za mesanje redosleda indexa u nizu.
+
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
 }
+
+//Generisemo broj za svaki par u nizu, broj mora biti jedinstven za svaki par. Svaki broj dodajemo u niz 2 puta, za svaku karticu po 1.
 
 for (i = 0; i < uniqueNumber; i++) {
   let randomNumber = Math.floor(Math.random() * 12);
@@ -79,7 +92,11 @@ for (i = 0; i < uniqueNumber; i++) {
   uniqueNumbers.push(randomNumber);
 }
 
+//Pozivamo funkciju da promesa niz sa jedinstvenim brojevima da se ne bi stvarale kartice jedna pored druge.
+
 shuffleArray(uniqueNumbers);
+
+//Pravimo zeljeni broj kartica, svaka kartica uzima prvi broj u promesanom nizu, ulazi u niz sa karticama i izbacuje broj uzet iz niza.
 
 for (i = 0; i < totalCards; i++) {
   const card = new Cards("black", uniqueNumbers[0]);
